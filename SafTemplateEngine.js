f3753ec6b027d684064da7d5132327c16a68cbd5
Scott Flaherty <Scott.Flaherty@mckesson.com>
2015-01-08 15:55:08 -0500

    Adding engine and example

---
+++ SafTemplateEngine.js
@@ -0,0 +1,32 @@
+// Safely create namespace
+var SAF = SAF || {};
+
+SAF.TemplateEngine = {};
+
+(function(engine) {
+    var re = /<%([^%>]+)?%>/g;
+    var reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g;
+    var code = 'var r=[];\n';
+
+    engine.execute = function (html, options) {
+        var cursor = 0;
+
+        code ='var r=[];\n';
+
+        while(match = re.exec(html)) {
+            add(html.slice(cursor, match.index))(match[1], true);
+            cursor = match.index + match[0].length;
+        }
+
+        add(html.substr(cursor, html.length - cursor));
+        code += 'return r.join("");';
+
+        return new Function(code.replace(/[\r\t\n]/g, '')).apply(options);
+    };
+
+    var add = function(line, js) {
+        js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
+            (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+        return add;
+    };
+}(SAF.TemplateEngine));
